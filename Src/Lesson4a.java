69cb5e2a6c39373c050a186e86f53d7edd83ef20
Katerina <dvernitskayae@gmail.com>
2023-06-04 23:50:54 +0300

    another message

--- Src/Lesson4a.java
+++ Src/Lesson4a.java
@@ -1,5 +1,9 @@
 public class Lesson4a {
     public static void main(String[] args) {
+
         System.out.println(" Hello git");
+        System.out.println(" Hello git from Kateryna");
+
     }
+
 }
